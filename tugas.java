8efb02dd924f673f73cf371bfa55a92c5d9392aa
anisaputrimasuku <163608872+anisaputrimasuku@users.noreply.github.com>
2024-03-29 16:13:51 +0900

    data saya

---
+++ tugas.java
@@ -0,0 +1,20 @@
+public static void main(string[] args) {
+    // membuat variabel 
+    string nama, alamat; 
+    int usia;
+    double tinggi;
+
+    // mengisi variabel
+    nama = "anisa";
+    alamat = "kotaraja";
+    usia = 21;
+    tinggi = 160cm;
+
+    // mencetak ke layar isi variabel
+    system.out.println("nama");
+    system.out.println("alamat; + alamat");
+    system.out.println("usia; + usia + "tahun");
+    system.out.println("tinggi; " + tinggi + tinggi + "cm");
+    }
+
+} 
